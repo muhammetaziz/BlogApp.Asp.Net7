68466dc76d12d18f80d1d328ae3c8444bad2c865
Muhammet Aziz AÇIKER <m.aziz.aciker@gmail.com>
2024-11-08 23:57:56 +0300

    theme

---
+++ BlogApp.Net7/Controllers/TestController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BlogApp.Net7.Controllers
+{
+    public class TestController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
